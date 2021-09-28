e09bed6a34899106e2902d6f8bfda524ee7ab161
harshil2801 <harshilshah280102@gmail.com>
2021-09-28 11:26:26 +0530

    MY commit

--- src/app/charts/chartjs/chartjs.component.ts
+++ src/app/charts/chartjs/chartjs.component.ts
@@ -2,37 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../../api.service";
 import { ViewChild, ElementRef } from '@angular/core';
 
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+// import * as am4core from "@amcharts/amcharts4/core";
+// import * as am4charts from "@amcharts/amcharts4/charts";
+// import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { Chart } from 'chart.js';
 
-let chart = am4core.create("chartdiv", am4charts.XYChart);
-chart.data = generateChartData();
-function generateChartData(){
-  var sentiment = []
-  this.rest
-        .getweeklysentiments(
-          JSON.stringify({
-            from: `2021-09-01`,
-            to: `2021-09-07`,
-          })
-        )
-        .subscribe((sentimentta) => {
-          sentimentta = JSON.parse(sentimentta);
-          console.log(sentimentta);
-          // var sentiment = [],
-          for(let item of Object.keys(sentimentta)){
-            sentimentta[item]['date'] = item;
-            sentiment.push(sentimentta[item]);
+import type { XYChart } from "@amcharts/amcharts4/charts";
+declare const am4core: typeof import("@amcharts/amcharts4/core");
+declare const am4charts: typeof import("@amcharts/amcharts4/charts");
+declare const am4themes_animated: typeof import("@amcharts/amcharts4/themes/animated").default;
 
-          }
-          // return sentiment;
-  })
-  return sentiment;
-}
-let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-dateAxis.renderer.minGridDistance = 50;
+// let chart = am4core.create("chartdiv", am4charts.XYChart);
+// chart.data = generateChartData();
+// function generateChartData(){
+//   var sentiment = []
+//   this.rest
+//         .getweeklysentiments(
+//           JSON.stringify({
+//             from: `2021-09-01`,
+//             to: `2021-09-07`,
+//           })
+//         )
+//         .subscribe((sentimentta) => {
+//           sentimentta = JSON.parse(sentimentta);
+//           console.log(sentimentta);
+//           // var sentiment = [],
+//           for(let item of Object.keys(sentimentta)){
+//             sentimentta[item]['date'] = item;
+//             sentiment.push(sentimentta[item]);
+
+//           }
+//           // return sentiment;
+//   })
+//   return sentiment;
+// }
+// let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+// dateAxis.renderer.minGridDistance = 50;
 
 @Component({
   selector: 'app-chartjs',
@@ -44,16 +49,43 @@ dateAxis.renderer.minGridDistance = 50;
 
 export class ChartjsComponent implements OnInit {
 
- // public chart : am4core.create("chartdiv", am4charts.XYChart);
+//  public chart : am4core.create("chartdiv", am4charts.XYChart);
+chart = am4core.create("chartdiv", am4charts.XYChart);
+public dateAxis :any
+//this.chart.data = generateChartData();
+generateChartData(){
+  var sentiment = []
+  this.rest
+        .getweeklysentiments(
+          JSON.stringify({
+            from: '2021-09-01',
+            to: '2021-09-07',
+          })
+        )
+        .subscribe((sentimentta) => {
+          sentimentta = JSON.parse(sentimentta);
+          console.log(sentimentta);
+          // var sentiment = [],
+          for(let item of Object.keys(sentimentta)){
+            sentimentta[item]['date'] = item;
+sentiment.push(sentimentta[item]);
 
-  @ViewChild('myCanvas')
-  public canvas: ElementRef;
-  public context: CanvasRenderingContext2D;
-  public chartType: string = 'line';
-  public chartData: any[];
-  public chartLabels: any[];
-  public chartColors: any[];
-  public chartOptions: any;
+          }
+          // return sentiment;
+  })
+  return sentiment;
+}
+// dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+// dateAxis.renderer.minGridDistance = 50;
+
+  // @ViewChild('myCanvas')
+  // public canvas: ElementRef;
+  // public context: CanvasRenderingContext2D;
+  // public chartType: string = 'line';
+  // public chartData: any[];
+  // public chartLabels: any[];
+  // public chartColors: any[];
+  // public chartOptions: any;
   
   // lineChartData = [{
   //   label: '# of Mixed Sentiment',
@@ -113,39 +145,43 @@ export class ChartjsComponent implements OnInit {
   constructor(public rest: ApiService) { }
 
   ngOnInit() {
+    this.chart.colors.step = 2;
+    this.chart.data = this.generateChartData();
+    this.dateAxis = this.chart.xAxes.push(new am4charts.DateAxis());
+    this.dateAxis.renderer.minGridDistance = 50;
 
-    
+    // this.chart.data = generateChartData();
     
       
 
-    this.rest
-        .getweeklysentiments(
-          JSON.stringify({
-            from: `2021-09-01`,
-            to: `2021-09-07`,
-          })
-        )
-        .subscribe((sentimentta) => {
-          sentimentta = JSON.parse(sentimentta);
-          console.log(sentimentta);
-          var sentiment = []
-          var psentiment = []
-          var nsentiment = []
-          var neusentiment = []
-          var msentiment = []
-          for(let item of Object.keys(sentimentta)){
-            sentiment.push(sentimentta[item])
+    // this.rest
+    //     .getweeklysentiments(
+    //       JSON.stringify({
+    //         from: `2021-09-01`,
+    //         to: `2021-09-07`,
+    //       })
+    //     )
+    //     .subscribe((sentimentta) => {
+    //       sentimentta = JSON.parse(sentimentta);
+    //       console.log(sentimentta);
+    //       var sentiment = []
+    //       var psentiment = []
+    //       var nsentiment = []
+    //       var neusentiment = []
+    //       var msentiment = []
+    //       for(let item of Object.keys(sentimentta)){
+    //         sentiment.push(sentimentta[item])
             // neusentiment.push(item['sentiment_score_neutral'])
             // nsentiment.push(item['sentiment_score_negative'])
             // psentiment.push(item['sentiment_score_positive'])
             // msentiment.push(item['created_at'])
-          }
+          // }
           //this.lineChartData[0]['key1'] = sentiment
           // this.plineChartData[0]['day1'] = psentiment
           // this.nlineChartData[0]['day1'] = nsentiment
           // this.neulineChartData[0]['day1'] = neusentiment
           // this.lineChartLabels[0]['day1'] = msentiment
-        });
+        // });
   }
 
 }
